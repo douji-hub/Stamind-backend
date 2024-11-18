73e24b22df42e09e00a73881dee613c6c7de7e01
Jim <jmjmjim1101@gmail.com>
2024-11-19 01:38:05 +0800

    feat: adding route

--- src/routes/routeHub.ts
+++ src/routes/routeHub.ts
@@ -1,9 +1,13 @@
 // routes/index.ts
 import express from 'express';
 import authRoutes from './authRoutes';
+import spaceRoutes from './spaceRoutes';
+import blockRoutes from './blockRoutes';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
+router.use('/space', spaceRoutes);
+router.use('/block', blockRoutes);
 
 export default router;
